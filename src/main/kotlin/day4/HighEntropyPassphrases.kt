f00508ae5227dcec0b43db713858e94cd7ef7052
Markus Spath <markus@hackr.de>
2022-06-23 06:31:00 +0200

    Day 4 Part 2

--- src/main/kotlin/day4/HighEntropyPassphrases.kt
+++ src/main/kotlin/day4/HighEntropyPassphrases.kt
@@ -5,12 +5,23 @@ import java.io.File
 fun main() {
     val input = File("data/day4/input.txt").readLines()
     breakfast(input)
+    lunch(input)
 }
 
 fun breakfast(passphrases: List<String>) {
     val valid = passphrases.filter { passphrase ->
         val list = passphrase.split(" ")
         // toSet conveniently does the job for us
+        val set = list.toSet()
+        list.size == set.size
+    }
+    println(valid.size)
+}
+
+fun lunch(passphrases: List<String>) {
+    val valid = passphrases.filter { passphrase ->
+        val list = passphrase.split(" ")
+            .map { it.toCharArray().sorted().joinToString("") }
         val set = list.toSet()
         list.size == set.size
     }
