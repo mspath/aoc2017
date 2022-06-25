76572564c741a40d57454de5ed44079ae94057da
Markus Spath <markus@hackr.de>
2022-06-25 20:12:32 +0200

    Day 7 Pt2

--- src/main/kotlin/day7/RecursiveCircus.kt
+++ src/main/kotlin/day7/RecursiveCircus.kt
@@ -24,8 +24,41 @@ fun children(input: List<String>, node: String): List<String> {
         .split(", ")
 }
 
+data class Program(val name: String, val weight: Int, val children: List<String>) {
+
+    fun totalWeight(map: Map<String, Program>): Int {
+        return if (children.isNullOrEmpty()) weight
+        else weight +
+                children.mapNotNull { map[it] }.map { it.totalWeight(map) }.sum()
+    }
+
+    companion object {
+        fun from(name: String, input: List<String>): Program {
+            val line = input.first { it.startsWith(name) }
+            val weight = line.substringAfter("(").substringBefore(")").toInt()
+            val children = line.substringAfter(" -> ", "").split(", ")
+            return Program(name, weight, children)
+        }
+    }
+}
+
+fun parseTower(input: List<String>): List<Program> {
+    val names = input.map { it.substringBefore(" ") }
+    return names.map { Program.from(it, input) }
+}
+
 fun lunch(input: List<String>) {
-    val root = breakfast(input)
-    println(root)
-    println(children(input, root))
+    val tower = parseTower(input)
+    val map = tower.associateBy { it.name }
+    map.values.filter { it.children.isNotEmpty() }.forEach { program ->
+        val weights = program.children.mapNotNull { map[it]?.totalWeight(map) }
+        if (weights.toSet().size > 1) {
+            println("--")
+            println(program)
+            println(weights)
+        }
+    }
+    // now we know the name of the problematic program and the difference
+    // let's just calculate it manually
+    map["apjxafk"]?.let { println(it.weight - 8) }
 }
