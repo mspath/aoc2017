fb74422ef330e09d24a2176aeaabad3783d040f2
Markus Spath <markus@hackr.de>
2022-06-29 07:51:03 +0200

    Day 12 pt2

--- src/main/kotlin/day12/DigitalPlumber.kt
+++ src/main/kotlin/day12/DigitalPlumber.kt
@@ -2,10 +2,12 @@ package day12
 
 import java.io.File
 
-
 fun main() {
     val input = File("data/day12/input.txt").readLines()
-    breakfast(input)
+    val resultBreakfast = breakfast(input)
+    println(resultBreakfast)
+    val resultLunch = lunch(input)
+    println(resultLunch)
 }
 
 fun getConnections(map: Map<Int, Set<Int>>, from: Int, visited: MutableSet<Int> = mutableSetOf()): Set<Int> {
@@ -16,11 +18,24 @@ fun getConnections(map: Map<Int, Set<Int>>, from: Int, visited: MutableSet<Int> 
     return visited
 }
 
-fun breakfast(pipes: List<String>) {
+fun breakfast(pipes: List<String>): Int {
     val map = pipes.map {
         val line = it.split(" <-> ")
         line.first().toInt() to line.last().split(", ").map { n -> n.toInt() }.toSet()
     }.toMap()
-    val result = getConnections(map, 0).size
-    println(result)
+    return getConnections(map, 0).size
+}
+
+fun lunch(pipes: List<String>): Int {
+    var map = pipes.map {
+        val line = it.split(" <-> ")
+        line.first().toInt() to line.last().split(", ").map { n -> n.toInt() }.toSet()
+    }.toMap()
+    var areas = 0
+    while (map.keys.isNotEmpty()) {
+        areas++
+        val pool = getConnections(map, map.keys.first())
+        map = map.filter { it.key !in pool }
+    }
+    return areas
 }
