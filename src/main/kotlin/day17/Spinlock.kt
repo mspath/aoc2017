355b0543de0533846ca37e2a681ef09e5fcd06ea
Markus Spath <markus@hackr.de>
2022-07-02 16:44:26 +0200

    Day 17 pt2

--- src/main/kotlin/day17/Spinlock.kt
+++ src/main/kotlin/day17/Spinlock.kt
@@ -1,12 +1,11 @@
 package day17
 
-
 fun main() {
-    val stepsSample = 3
     val steps = 316
-    val cyclesSample = 9
-    val cycles = 2017
-    breakfast(steps, cycles)
+    val cyclesBreakfast = 2017
+    val cycles = 50_000_000
+    breakfast(steps, cyclesBreakfast)
+    lunch(steps, cycles)
 }
 
 fun breakfast(steps: Int, cycles: Int) {
@@ -17,4 +16,30 @@ fun breakfast(steps: Int, cycles: Int) {
         spinlock.add(++position, it + 1)
     }
     println(spinlock[position + 1])
+}
+
+// brute force won't word,
+// but we just need to keep up with the index of 0 and which number is behind
+// - index of 0 shifts by one every time a value is inserted before it
+// - the number behind gets updated every time a value is inserted at index0 + 1
+fun lunch(steps: Int, cycles: Int) {
+    var position = 0
+    var index0 = 0
+    var following0 = Int.MIN_VALUE
+    var size = 1
+    repeat(cycles) { next ->
+        val insertPosition = (position + steps) % size
+        val diff0 = index0 - insertPosition
+        when {
+            diff0 < index0 -> {
+                index0++
+            }
+            diff0 == index0 -> {
+                following0 = next
+            }
+        }
+        size++
+        position = insertPosition + 1
+    }
+    println(following0)
 }
