ff244d95115ea6ebdd3cac0fe1476d7aa1d7949e
Markus Spath <markus@hackr.de>
2022-07-04 14:44:09 +0200

    Day 22 pt2 incomplete

--- src/main/kotlin/day22/SporificaVirus.kt
+++ src/main/kotlin/day22/SporificaVirus.kt
@@ -4,9 +4,12 @@ import java.io.File
 
 typealias Grid = MutableSet<Pair<Int, Int>>
 
+typealias GridEvolved = MutableSet<Node>
+
 fun main() {
-    val input = File("data/day22/input.txt").readLines()
-    breakfast(input)
+    val input = File("data/day22/sample.txt").readLines()
+    //breakfast(input)
+    lunch(input)
 }
 
 fun parseGrid(input: List<String>): Grid {
@@ -63,20 +66,29 @@ data class Virus(var position: Pair<Int, Int> = Pair(0, 0),
                  var counter: Int = 0) {
 
     fun turnRight() {
-        when (direction) {
-            Direction.UP -> direction = Direction.RIGHT
-            Direction.RIGHT -> direction = Direction.DOWN
-            Direction.DOWN -> direction = Direction.LEFT
-            Direction.LEFT -> direction = Direction.UP
+        direction = when (direction) {
+            Direction.UP -> Direction.RIGHT
+            Direction.RIGHT -> Direction.DOWN
+            Direction.DOWN -> Direction.LEFT
+            Direction.LEFT -> Direction.UP
         }
     }
 
     fun turnLeft() {
-        when (direction) {
-            Direction.UP -> direction = Direction.LEFT
-            Direction.RIGHT -> direction = Direction.UP
-            Direction.DOWN -> direction = Direction.RIGHT
-            Direction.LEFT -> direction = Direction.DOWN
+        direction = when (direction) {
+            Direction.UP -> Direction.LEFT
+            Direction.RIGHT -> Direction.UP
+            Direction.DOWN -> Direction.RIGHT
+            Direction.LEFT -> Direction.DOWN
+        }
+    }
+
+    fun reverse() {
+        direction = when (direction) {
+            Direction.UP -> Direction.DOWN
+            Direction.RIGHT -> Direction.LEFT
+            Direction.DOWN -> Direction.UP
+            Direction.LEFT -> Direction.RIGHT
         }
     }
 
@@ -97,5 +109,101 @@ fun breakfast(input: List<String>) {
         grid.burst(virus)
     }
     grid.display()
+    println(virus.counter)
+}
+
+enum class State {
+    CLEAN,
+    WEAKENED,
+    INFECTED,
+    FLAGGED
+}
+
+data class Node(val x: Int, val y: Int, var state: State)
+
+fun parseGridEvolved(input: List<String>): GridEvolved {
+    val grid: MutableSet<Node> = mutableSetOf()
+    val size = input.size
+    val depth = size / 2
+    input.forEachIndexed { indexY, line ->
+        val y = indexY - depth
+        line.forEachIndexed { indexX, c ->
+            val x = indexX - depth
+            if (c == '#') grid.add(Node(x, y, State.INFECTED))
+        }
+    }
+    return grid
+}
+
+fun GridEvolved.displayEvolved() {
+    val minX = this.minOf { it.x } - 1
+    val maxX = this.maxOf { it.x } + 1
+    val minY = this.minOf { it.y } - 1
+    val maxY = this.maxOf { it.y } + 1
+
+    (minY..maxY).forEach { y ->
+        (minX..maxX).forEach { x ->
+            val clean = this.filter { it.x == x && it.y == y }.isEmpty()
+            if (clean) {
+                print('.')
+            } else {
+                val node = this.first { it.x == x && it.y == y }
+                when (node.state) {
+                    State.WEAKENED -> {
+                        print('W')
+                    }
+                    State.INFECTED -> {
+                        print('#')
+                    }
+                    State.FLAGGED -> {
+                        print('F')
+                    }
+                    else -> {}
+                }
+            }
+        }
+        print('\n')
+    }
+}
+
+fun GridEvolved.burstEvolved(virus: Virus) {
+    val clean = this.filter { it.x == virus.position.first && it.y == virus.position.second }.isEmpty()
+    if (clean) {
+        virus.turnLeft()
+        this.add(Node(virus.position.first, virus.position.second, State.WEAKENED))
+    }
+    else {
+        val node = this.first { it.x == virus.position.first && it.y == virus.position.second }
+        when (node.state) {
+            State.WEAKENED -> {
+                node.state = State.INFECTED
+                virus.counter++
+            }
+            State.INFECTED -> {
+                node.state = State.FLAGGED
+                virus.turnRight()
+            }
+            State.FLAGGED -> {
+                node.state = State.CLEAN
+                this.remove(node)
+                virus.reverse()
+            }
+            else -> {}
+        }
+    }
+    virus.move()
+}
+
+// TODO unfinished. prob. an off by one error
+fun lunch(input: List<String>) {
+    val grid = parseGridEvolved(input)
+    val virus = Virus()
+    repeat(12) {
+        grid.burstEvolved(virus)
+        println(it)
+        grid.displayEvolved()
+        println("---")
+    }
+    grid.displayEvolved()
     println(virus.counter)
 }
